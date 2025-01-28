339230ac270093cf7f9e218095ab4c994dd04b35
Adriano Gualandi (NR) Staff Mar 2023 <ag2347@earth-chaos.esc.cam.ac.uk>
2025-01-28 16:04:17 +0000

    manually solve inconsistentcies

--- driver.jl
+++ driver.jl
@@ -39,15 +39,9 @@ solutions_dates = ["2024-08-01/",
                    "2024-08-30/",
                    "2024-08-31/",
                    ]
-<<<<<<< HEAD
-solutions_dates = ["2025-01-06/"]
-solution_date = "2025-01-24/"
-# for solution_date in solutions_dates
-=======
 solution_dates = ["2025-01-26/"]
 # solution_date = "2024-08-06/"
 for solution_date in solutions_dates
->>>>>>> 4017893 (Update ENV path)
     println(" ")
     println(solution_date)
     #######################
